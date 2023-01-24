38dfdbb617de79bb000eefe6e2b1afd4c7f850ef
iammilankhati <iammilankhati@gmail.com>
2023-01-24 18:21:23 +0545

    variables and datatypes

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-import numpy
+names = 'Milan, suman'  #string
+male = True #boolean
+age = 24 #integer
+height = 5.5 #float
+
+
+
+
+names[-3:]
+print(len(names))
+# names[0] = "N"    #stings are immutable
+
+
+print(f'{names} are friends')
+
+
+x, y,z = 34,323,12
+
+
+user_input = (input('please enter the number'))
+print(type(user_input))
+
+
+
+
+
